435f5f3a708830b1dd6213aecab2dcb2e25953b9
fabianVeliz <velizfabianhoracio@gmail.com>
2015-05-05 11:46:34 -0300

    Updates application.rb file

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ Bundler.require(*Rails.groups)
 
 module Toptopic
   class Application < Rails::Application
+    config.serve_static_assets = true
     config.generators do |g|
       g.template_engine :slim
 
